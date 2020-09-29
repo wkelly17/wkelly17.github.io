84f10878a44bb04cfcedbb20424e0bdddedeb6f2
Will Kelly <willkelly9617@gmail.com>
2020-09-29 16:19:19 -0500

    Fixed svg overflow of hamburger animation

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const hamburger = document.querySelector(".hamburger-menu");
-const topNavList = document.querySelector(".top-nav-list");
-const aboutSection = document.querySelector(".about-section");
-const bottomHamburger = document.querySelector(".hamburger-line-bottom");
-const topHamburger = document.querySelector(".hamburger-line-top");
-const midHamburger = document.querySelector(".hamburger-line-middle");
+const hamburger = document.querySelector('.hamburger-menu');
+const topNavList = document.querySelector('.top-nav-list');
+const aboutSection = document.querySelector('.about-section');
+const bottomHamburger = document.querySelector('.hamburger-line-bottom');
+const topHamburger = document.querySelector('.hamburger-line-top');
+const midHamburger = document.querySelector('.hamburger-line-middle');
 
-hamburger.addEventListener("click", openNav);
+hamburger.addEventListener('click', openNav);
+hamburger.addEventListener('touchStart', openNav);
 
 function openNav(event) {
-  topNavList.classList.toggle("top-nav-list-open");
+  topNavList.classList.toggle('top-nav-list-open');
   hamburgerToggle();
 }
 
 function hamburgerToggle(event) {
-  console.log("clicked");
-  bottomHamburger.classList.toggle("bottom-ham-open");
-  topHamburger.classList.toggle("top-ham-open");
-  midHamburger.classList.toggle("middle-ham-open");
+  console.log('clicked');
+  bottomHamburger.classList.toggle('bottom-ham-open');
+  topHamburger.classList.toggle('top-ham-open');
+  midHamburger.classList.toggle('middle-ham-open');
 }
